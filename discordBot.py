7c83bf9ef3dc94e74b9ff2a72e07e23a8cc9c935
Deondre De Freitas <38331348+Deondrede@users.noreply.github.com>
2020-07-13 23:37:27 -0400

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -42,4 +42,4 @@ async def on_message(message):
     if message.content.startswith("!map"):
         await message.channel.send(file=discord.File("Hollow Knight Map.jpg"))      #attaching files 
        
-client.run('NTkyODgyOTU4NDc4ODY4NDkx.Xwoi9w.wCxIno4WdDJ3pDLtwyQSx1Hrssw')
+client.run('')
